fcbcfa9ad3bf1a417ee8fd0e98fd79122e95c2ae
Philip Nilsson <philip@nilsson.gi>
2020-07-27 19:24:25 +0100

    Added responsiveness to nav bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import './NavBar.scss';
 import { NavLink } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import FavouriteIcon from '@material-ui/icons/Favorite';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 
 export default function NavBar() {
     return (
         <div className='NavBar'>
             <div className='NavItem'>
+                <SearchIcon></SearchIcon>
                 <NavLink exact to='/'>Search</NavLink>
             </div>
             <div className='NavItem'>
+                <FavouriteIcon></FavouriteIcon>
                 <NavLink to='/favourites'>Favourites</NavLink>
             </div>
             <div className='NavItem'>
+                <PlaylistPlayIcon></PlaylistPlayIcon>
                 <NavLink to='/watch-later'>Watch Later</NavLink>
             </div>
         </div>
