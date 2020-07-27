ff13624eebc374c947b762dd11483ce94df1af5d
Philip Nilsson <philip@nilsson.gi>
2020-07-27 18:49:23 +0100

    Added some styling to navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './NavBar.css';
+import './NavBar.scss';
 import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
     return (
         <div className='NavBar'>
             <div className='NavItem'>
-                <NavLink to='/'>Search</NavLink>
+                <NavLink exact to='/'>Search</NavLink>
             </div>
             <div className='NavItem'>
                 <NavLink to='/favourites'>Favourites</NavLink>
