71f4e6c69871ae53f848fc408a52ae15b1682e71
Philip Nilsson <philip@nilsson.gi>
2020-07-27 19:41:14 +0100

    Fixed navbar staying at top or bottom of screen depending on screen width

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import NavBar from './components/NavBar/NavBar';
 import { BrowserRouter as Router } from 'react-router-dom';
 
