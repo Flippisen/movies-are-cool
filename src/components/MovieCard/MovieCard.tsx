4c131806d6bc68241237462b6560a0c2f98911da
Philip Nilsson <philip@nilsson.gi>
2020-07-28 21:51:55 +0100

    Added icons for favouriting and watch later

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -3,6 +3,8 @@ import { Movie } from '../../models/movie';
 import './MovieCard.scss';
 import BrokenImageIcon from '@material-ui/icons/BrokenImage';
 import StarRateIcon from '@material-ui/icons/StarRate';
+import FavouriteOutlineIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
 
 type Props = {
     movie: Movie
@@ -33,6 +35,10 @@ export default (props: Props) => {
                             {movie.voteAverage}
                             <StarRateIcon></StarRateIcon>
                         </div>
+                    </div>
+                    <div className='ActionItems'>
+                        <FavouriteOutlineIcon></FavouriteOutlineIcon>
+                        <PlaylistAddIcon></PlaylistAddIcon>
                     </div>
                 </div>
             </div>
