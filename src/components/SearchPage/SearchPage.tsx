bc15fb50fc1e8c52219fb69658dd81c293d091e1
Philip Nilsson <philipn@apadmi.com>
2020-08-01 16:17:12 +0100

    Added a loading state to search page

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -3,16 +3,15 @@ import { apiUrl } from '../../services/api';
 import { useSearchState } from '../../contexts/SearchContext';
 import SearchBar from './SearchBar/SearchBar';
 import { useDebounce } from '../../hooks/useDebounce';
-import MovieCard from '../MovieCard/MovieCard';
 import './SearchPage.scss';
 import { Movie } from '../../models/movie';
-import SearchResults from './SearchResults/SearchResults';
+import SearchResults from '../MovieList/SearchResults';
 
 export default () => {
     const [page, setPage] = useState(1);
     const [numPages, setNumPages] = useState(1);
     const [totalResults, setTotalResults] = useState(0);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     const { setSearchResults, searchTerm, searchResults } = useSearchState();
     const debouncedSearchTerm = useDebounce<string>(searchTerm, 150);
 
@@ -27,6 +26,7 @@ export default () => {
             return;
         }
         const getSearchResults = async () => {
+            setIsLoading(true);
             const results = await fetch(
                 apiUrl('/search/movie', { query: debouncedSearchTerm, page: page}),
                 {
@@ -58,18 +58,35 @@ export default () => {
         getSearchResults();
     }, [page, debouncedSearchTerm]);
 
+    const loadingDiv = () => {
+        return <div className='loading'>Loading...</div>
+    }
+
+    const searchResultsDiv = () => {
+        return (
+            <div>
+                <SearchResults></SearchResults>
+                { 
+                    totalResults > 0 && totalResults !== searchResults.length &&
+                    <div className='ButtonContainer'>
+                        <div className='Button LoadMore' onClick={e => setPage(page + 1)}>
+                            Load more
+                                </div>
+                    </div>
+                }
+            </div>
+        )
+    }
+
     return (
         <div>
             <SearchBar></SearchBar>
-            <SearchResults></SearchResults>
             {
-                totalResults > 0 && totalResults !== searchResults.length &&
-                <div className='ButtonContainer'>
-                    <div className='Button LoadMore' onClick={e => setPage(page + 1)}>
-                        Load more
-                    </div>
-                </div>
+                isLoading ?
+                    loadingDiv() :
+                    searchResultsDiv()
             }
+
         </div>
     )
 }
