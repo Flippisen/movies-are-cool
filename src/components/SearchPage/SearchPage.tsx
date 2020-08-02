187db3dbf10281d4af806e8f862879d6a87c45c4
Philip Nilsson <philip@nilsson.gi>
2020-08-02 17:33:54 +0100

    Search results are now cancelled if search term changes during request

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -25,37 +25,47 @@ export default () => {
         if (debouncedSearchTerm === '') {
             return;
         }
+        const abortController = new AbortController();
+        let cancelled = false;
         const getSearchResults = async () => {
             setIsLoading(true);
             const results = await fetch(
                 apiUrl('/search/movie', { query: debouncedSearchTerm, page: page}),
                 {
-                    method: 'GET'
+                    method: 'GET',
+                    signal: abortController.signal
                 }
             );
             const response = await results.json();
-            setNumPages(response['total_pages']);
-            setTotalResults(response['total_results']);
-            const newResults = response['results'].map((result: any) => new Movie(
-                result.poster_path,
-                result.adult,
-                result.overview,
-                result.release_date,
-                result.genre_ids,
-                result.id,
-                result.original_title,
-                result.original_language,
-                result.title,
-                result.backdrop_path,
-                result.popularity,
-                result.vote_count,
-                result.video,
-                result.vote_average
-            ))
-            setSearchResults([...searchResults, ...newResults]);
-            setIsLoading(false);
+            if (!cancelled) {
+                setNumPages(response['total_pages']);
+                setTotalResults(response['total_results']);
+                const newResults = response['results'].map((result: any) => new Movie(
+                    result.poster_path,
+                    result.adult,
+                    result.overview,
+                    result.release_date,
+                    result.genre_ids,
+                    result.id,
+                    result.original_title,
+                    result.original_language,
+                    result.title,
+                    result.backdrop_path,
+                    result.popularity,
+                    result.vote_count,
+                    result.video,
+                    result.vote_average
+                ))
+                setSearchResults([...searchResults, ...newResults]);
+                setIsLoading(false);
+            }
+
         }
         getSearchResults();
+        return () => {
+            cancelled = true;
+            abortController.abort()
+        }
     }, [page, debouncedSearchTerm]);
 
     const loadingDiv = () => {
