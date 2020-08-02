a18d134142e4595eed11d85e253fe9bfb8af8713
Philip Nilsson <philip@nilsson.gi>
2020-08-02 16:50:37 +0100

    Movie details page now gets details from movie api

--- src/components/MovieDetailsPage/MovieDetailsPage.tsx
+++ src/components/MovieDetailsPage/MovieDetailsPage.tsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import { Movie, MovieDetails } from '../../models/movie';
+import { apiUrl } from '../../services/api';
 
 export default () => {
+    const [result, setResult] = useState<Movie | undefined>(undefined);
+    const [isLoading, setIsLoading] = useState(false);
     const { id } = useParams();
 
-    return <div>
+    useEffect(() => {
+        const getMovieById = async () => {
+            setIsLoading(true);
+            const result = await fetch(
+                apiUrl(`/movie/${id}`),
+                {
+                    method: 'GET'
+                }
+            )
+            const response = await result.json();
+            const movieResult = new MovieDetails(
+                response.poster_path,
+                response.adult,
+                response.overview,
+                response.release_date,
+                response.genre_ids,
+                response.id,
+                response.original_title,
+                response.original_language,
+                response.title,
+                response.backdrop_path,
+                response.popularity,
+                response.vote_count,
+                response.video,
+                response.vote_average,
+                response.belongs_to_collection,
+                response.budget,
+                response.genres,
+                response.homepage,
+                response.imdb_id,
+                response.production_companies,
+                response.revenue,
+                response.runtime,
+                response.spokenLanguage,
+                response.status,
+                response.tagline
+            );
+            setResult(movieResult);
+            setIsLoading(false);
+            console.log(movieResult);
+        }
+        getMovieById();
+    }, [id])
+
+    return <div className='Card'>
         Movie details
     </div>
 }
