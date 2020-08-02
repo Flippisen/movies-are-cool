520a1d8c265871871215f82d318a126c42c3978f
Philip Nilsson <philip@nilsson.gi>
2020-08-02 16:24:49 +0100

    Movie details page now gets id from path params

--- src/components/MovieDetailsPage/MovieDetailsPage.tsx
+++ src/components/MovieDetailsPage/MovieDetailsPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 export default () => {
+    const { id } = useParams();
+
     return <div>
         Movie details
     </div>
