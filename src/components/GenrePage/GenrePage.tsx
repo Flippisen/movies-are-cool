c6dd4be803c0790e33276e6985ae2bd44561437f
Philip Nilsson <philip@nilsson.gi>
2020-08-18 19:44:30 +0100

    Added a function to get movie results

--- src/components/GenrePage/GenrePage.tsx
+++ src/components/GenrePage/GenrePage.tsx
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useGenreState } from '../../contexts/GenreContext';
 import './GenrePage.scss';
 import GenreDropdown from './GenreDropdown/GenreDropdown';
+import { makeApiCall, ApiMethods } from '../../services/api';
+import { Movie } from '../../models/movie';
 
 export default () => {
-    const { genreList, setGenreList, selectedGenres, setSelectedGenres } = useGenreState();
+    const { selectedGenres, movieResults, setMovieResults, currentPage, setCurrentPage } = useGenreState();
     const [isLoading, setIsLoading] = useState(false);
+    
+    const onSelectedGenreChangeGetNewResults = (selectedGenres: number[]) => {
+        if (!selectedGenres || selectedGenres.length > 0) {
+            setMovieResults([]);
+            setCurrentPage(1);
+        }
+        const getListOfGenres = async () => {
+            setIsLoading(true);
+            const response = await makeApiCall('/discover/movie', ApiMethods.GET, { 'with_genres': selectedGenres.join(',')});
+            const newResults = response['results'].map(Movie.fromResponse)
+            setMovieResults(newResults);
+            setIsLoading(false);
+        }
+        getListOfGenres();
+    }
+
+    useEffect(() => {
+        onSelectedGenreChangeGetNewResults(selectedGenres);
+    }, [selectedGenres])
 
     return <div className='genre-page'>
         <div className='header-row'>
