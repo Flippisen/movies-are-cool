54fc9a0b69c6516f551dd9281ddc6287edeab044
Philip Nilsson <philip@nilsson.gi>
2020-08-19 16:35:16 +0100

    Added some sort options to be used in dropdown

---
+++ src/components/GenrePage/SortDropdown/SortOptions.ts
@@ -0,0 +1,19 @@
+class SortOption {
+    public id: string;
+    public displayValue: string;
+
+    constructor(id: string, displayValue: string) {
+        this.id = id;
+        this.displayValue = displayValue;
+    }
+}
+
+export const SortOptions: SortOption[] = [
+    new SortOption('popularity.asc', 'Popularity Asc'),
+    new SortOption('popularity.desc', 'Popularity Desc'),
+    new SortOption('release_date.asc', 'Release Date Asc'),
+    new SortOption('release_date.desc', 'Release Date Desc'),
+    new SortOption('revenue.asc', 'Revenue Asc'),
+    new SortOption('revenue.desc', 'Revenue Desc'),
+
+]
