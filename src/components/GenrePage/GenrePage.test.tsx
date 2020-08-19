1306088386718c2d75f731b289bb4b231cd9d206
Philip Nilsson <philip@nilsson.gi>
2020-08-19 16:22:53 +0100

    Changed test to have one test for testing both forwards and backwards arrows, and also fixed this test

--- src/components/GenrePage/GenrePage.test.tsx
+++ src/components/GenrePage/GenrePage.test.tsx
@@ -50,61 +50,33 @@ test('When genre list is updated, fetch request should be made', async () => {
     await waitFor(() => expect(window.fetch).toHaveBeenCalledTimes(2));
 });
 
-// test('Clicking on next page button should fetch more data', async () => {
-//     window.fetch = (window.fetch as jest.Mock).mockImplementation(() => {
-//         return {
-//             ok: true,
-//             status: 200,
-//             json: async () => ({
-//                 'total_results': 10,
-//                 'total_pages': 3,
-//                 results: []
-//             })
-//         }
-//     });
+test('Clicking on next and previous page buttons should call fetch', async () => {
+    window.fetch = (window.fetch as jest.Mock).mockImplementation(() => {
+        return {
+            ok: true,
+            status: 200,
+            json: async () => ({
+                'total_results': 10,
+                'total_pages': 3,
+                results: []
+            })
+        }
+    });
 
-//     await waitFor(() => expect(window.fetch).toHaveBeenCalled());
-//     const genreDropdown = screen.getByTestId('genre-dropdown-input');
-//     if (!genreDropdown) {
-//         throw new Error('test failed');
-//     }
+    await waitFor(() => expect(window.fetch).toHaveBeenCalled());
+    const genreDropdown = screen.getByTestId('genre-dropdown-input');
+    if (!genreDropdown) {
+        throw new Error('test failed');
+    }
 
-//     selectMaterialUiSelectOption(genreDropdown, 'test-genre');
+    selectMaterialUiSelectOption(genreDropdown, 'test-genre');
 
 
-//     await waitFor(() => expect(window.fetch).toHaveBeenCalledTimes(2));
-//     const nextPageIcon = screen.getByRole('Go to next page');
-//     fireEvent.click(nextPageIcon);
-//     await waitFor(() => expect(window.fetch).toHaveBeenCalledTimes(3));
-// });
-
-// test('Clicking on previous page button should not call fetch', async () => {
-//     window.fetch = (window.fetch as jest.Mock).mockImplementation(() => {
-//         return {
-//             ok: true,
-//             status: 200,
-//             json: async () => ({
-//                 'total_results': 10,
-//                 'total_pages': 3,
-//                 results: []
-//             })
-//         }
-//     });
-
-//     await waitFor(() => expect(window.fetch).toHaveBeenCalled());
-//     const genreDropdown = screen.getByTestId('genre-dropdown');
-//     fireEvent.click(genreDropdown);
-//     const firstGenreInDropdown = genreDropdown.firstChild;
-//     if (!firstGenreInDropdown) {
-//         throw new Error('Test failed');
-//     }
-//     fireEvent.click(firstGenreInDropdown);
-//     await waitFor(() => expect(window.fetch).toHaveBeenCalled());
-//     const nextPageIcon = screen.getByRole('Go to next page');
-//     fireEvent.click(nextPageIcon);
-//     await waitFor(() => expect(window.fetch).toHaveBeenCalled());
-
-//     const previousPageIcon = screen.getByAltText('Go to previous page');
-//     fireEvent.click(previousPageIcon);
-//     await waitFor(() => expect(window.fetch).not.toHaveBeenCalled());
-// });
+    await waitFor(() => expect(window.fetch).toHaveBeenCalledTimes(2));
+    const nextPageIcon = screen.getByLabelText('Go to next page');
+    fireEvent.click(nextPageIcon);
+    await waitFor(() => expect(window.fetch).toHaveBeenCalledTimes(3));
+    const previousPageIcon = screen.getByLabelText('Go to previous page');
+    fireEvent.click(previousPageIcon);
+    await waitFor(() => expect(window.fetch).toHaveBeenCalledTimes(4));
+});
